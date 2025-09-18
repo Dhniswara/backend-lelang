17a5a80a4548053d2578c4a63001ff46808ef523
Dhniswara <dhaniswarafadhlurahman@gmail.com>
2025-09-18 16:54:33 +0700

    add email to nipl_transaction migration & model

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NiplController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HargaBidController;
 use App\Http\Controllers\LelangBarangController;
 use App\Http\Controllers\NiplCheckoutController;
-use App\Http\Controllers\Api\ApiCheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -95,13 +95,13 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     // Barang
 
     // Detail barang
-    Route::get('/checkout/item/{id}', [ApiCheckoutController::class, 'showItem']);
+    Route::get('/checkout/item/{id}', [CheckoutController::class, 'showItem']);
 
     // Daftar transaksi user login
-    Route::get('/checkout/transactions', [ApiCheckoutController::class, 'transactions']);
+    Route::get('/checkout/transactions', [CheckoutController::class, 'transactions']);
 
     // Buat pembayaran (generate invoice)
-    Route::post('/checkout/payment', [ApiCheckoutController::class, 'payment']);
-});
+    Route::post('/checkout/payment', [CheckoutController::class, 'payment']);
 
+});
 Route::post('/notification/webhook', [NiplCheckoutController::class, 'webhook']);
--- run-cron.sh
+++ run-cron.sh
@@ -5,7 +5,7 @@ set -e
 echo "Running the scheduler..."
 
 # Execute the Laravel scheduler command
-/usr/bin/php artisan schedule:run --verbose --no-interaction &
+/usr/bin/php artisan schedule:work --verbose --no-interaction &
 
 # Wait for 60 seconds before repeating, as the scheduler checks for due tasks every minute
 sleep 60
